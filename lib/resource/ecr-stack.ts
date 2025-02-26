49afb16cd3d1e026fe371769b58d42f6a872b811
Um e Habiba <um-e.habiba@zeiss.com>
2025-02-26 17:19:37 +0100

    fix from repo to image name

--- lib/resource/ecr-stack.ts
+++ lib/resource/ecr-stack.ts
@@ -24,7 +24,7 @@ export class EcrStack extends Stack {
     })
 
     new ECRDeployment(this, 'DeplyDockerImageToEcr', {
-      src: new DockerImageName(`${props.config.repoName}:${props.config.imageTag}`),
+      src: new DockerImageName(`${props.config.imageName}:${props.config.imageTag}`),
       dest: new DockerImageName(`${ecrRepo.repositoryUri}:${props.config.imageTag}`),
     })
   }
